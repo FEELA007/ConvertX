fc8595b76d89dff33aa26d099df8f8aeaf477105
karthik jammula <JAMMULEKARTHIK@GMAIL.COM>
2026-06-06 11:23:49 +0530

    Update image-compress.js

--- html/js/image-compress.js
+++ html/js/image-compress.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
             progressBar.style.width = "20%";
             progressText.textContent = "Uploading image...";
 
-            const response = await fetch("https://convertx-cfki.onrender.com/compress_image", { method: "POST", body: formData });
+            const response = await fetch("https://convertx-aa2j.onrender.com/compress_image", { method: "POST", body: formData });
             if (!response.ok) {
                 const error = await response.text();
                 alert(error);
